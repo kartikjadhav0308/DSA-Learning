50eb05ab43f3116dc716105ce52240606e81ef7c
kartikjadhav0308 <kartikjadhav0308@gmail.com>
2026-05-06 11:45:27 +0530

    All changes are done for basic sorting

--- Insertion_Sort.java
+++ Insertion_Sort.java
@@ -1,22 +1,23 @@
 public class Insertion_Sort {
     public static void main(String[] args) {
-        //we have to part sorted and unsorted
+        // we have to part sorted and unsorted
         // 7 | 8 3 1 2
         // 7 8 | 3 1 2
-        int[] arr = {7,8,3,1,2};
+        int[] arr = { 7, 8, 3, 1, 2 };
 
-        for(int i=1;i<arr.length;i++){
-            int j=i;
-            while(j>0 && arr[j]<arr[j-1]){
-                int temp = arr[j];
-                arr[j] = arr[j - 1];
-                arr[j - 1] = temp;
+        for (int i = 1; i < arr.length; i++) {
+            int current = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && current < arr[j]) {
+                arr[j + 1] = arr[j];
                 j--;
             }
+            arr[j + 1] = current;
         }
-        
-        for(int i:arr){
-            System.out.print(i+" ");
+
+        for (int i : arr) {
+            System.out.print(i + " ");
         }
     }
 }
